d07f6b482761d759709b8453ecfa785c7edf6262
Timothy Lau <git@timlau.me>
2026-03-16 03:47:00 +0800

    feat: add Button component and integrate into App

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from './components/ui/button'
+
 function App(): React.JSX.Element {
-  return <></>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
